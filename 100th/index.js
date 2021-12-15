6b421d635dcb51b17c89a15b257d215994af07b9
mujinlu <1826261786@qq.com>
2021-12-15 10:21:08 +0800

    Update index.js

--- 100th/index.js
+++ 100th/index.js
@@ -54,7 +54,7 @@
     }), 2 * commentTime);
   };
 
-  $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>跳过动画</a>\n    <a style='color: #fff' href='http://drlu.me/ownpage/yulu/'>重新播放</a>\n  </div>");
+  $('body').append("  <style id=\"style-tag\"></style>\n<span id=\"echo\"></span>\n<span id=\"heart\"><i></i></span>\n<pre id=\"style-text\"></pre>\n  <div style = 'position: fixed;left: 30px;bottom: 10px;'>\n    <a id='skipAnimation'>跳过动画</a>\n    <a style='color: #fff' href='http://drlu.me/yulu/100th'>重新播放</a>\n  </div>");
 
   commentTime = 60;
 
